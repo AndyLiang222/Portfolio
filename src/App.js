62b16ac01d33ba1da61aee02172f9151881f48b5
Andy <349562769@gapps.yrdsb.ca>
2022-09-06 11:46:11 -0400

    hashrouter

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import { BrowserRouter as Router,Routes, Route, Link } from 'react-router-dom';
+import { HashRouter as Router,Routes, Route, Link } from 'react-router-dom';
 import About from './About';
 import Projects from './Projects';
 import Contact from './Contact';
